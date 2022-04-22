91650841a9202e1b11ea6235a31fca2b8def3a2b
Naohiro Tsuji <587586+Naohiro2g@users.noreply.github.com>
2022-04-22 21:26:51 +0900

    46 mil complete

--- README.md
+++ README.md
@@ -7,16 +7,16 @@ wikipedia用 ラズパイ累計出荷台数のグラフ作成
 The graph is used on:
 
 - <https://ja.wikipedia.org/wiki/Raspberry_Pi>
+- <https://en.wikipedia.org/wiki/Raspberry_Pi>
 - <https://fr.wikipedia.org/wiki/Raspberry_Pi>
 - <https://it.wikipedia.org/wiki/Raspberry_Pi>
 - <https://zh.wikipedia.org/wiki/%E6%A0%91%E8%8E%93%E6%B4%BE>
 
-
-
-### matplotlibのフォントキャッシュをクリアする方法
+## matplotlibのフォントキャッシュをクリアする方法
 
 新しいフォントをインストールした後、キャッシュディレクトリを探して消去する必要がある。
-```
+
+```python
 import shutil
 import matplotlib
 
--- plot.py
+++ plot.py
@@ -1,64 +1,81 @@
-# version 2021-02-14
+# version 2022-04-22
 # wikipedia  https://ja.wikipedia.org/wiki/Raspberry_Pi
-# 累計出荷台数は2013年10月31日までで200万台[7]、2014年6月11日までで300万台[8]、2015年2月18日までで500万台[9]、
-# 2015年10月13日までで700万台[10]、2016年2月29日までで800万台[11]、2016年9月8日までで1,000万台[12]、
-# 2016年11月25日までで1,100万台[13]と、3年間で約1,000万台のペースであった。
-# その後、累計出荷台数は、ペースが上がり、2018年3月14日までで1,900万台[14]、 2019年12月14日までで3,000万台[15]、
-# 2021年1月21日までで3,700万台[16]と 1年で約600万台の直線的な増加となっている。
+
 
 import datetime as dt
-
+import matplotlib
 import matplotlib.pyplot as plt
-# import datetime
+import matplotlib.backends.backend_agg
+
+# Create a canvas with Agg backend then save it as a file.
+#     canvas = matplotlib.backends.backend_agg.FigureCanvasAgg(fig)
+#     canvas.print_figure(file_name, dpi=300)
+#     print(file_name + ' has been saved.')
+# You can show the plot on the display, too.
+#     show.plt
+
+
+date_time_now = dt.datetime.now().strftime('%Y%m%d%H%M%S')
+#file_name = 'images/Raspberry Pi Cumulative Shipment Units' + date_time_now + '.png'
+file_name = 'images/Raspberry Pi Cumulative Shipment Units' + '.png'
+# for Google Colab environment
+# file_name = '/content/images/RaspberryPiCumulativeShipmentUnits_' + date_time_now + '.png'
+
 
 date_values = (
-    "2012/2/29", 0,
-    "2013/10/31", 2000000,
-    "2014/6/11", 3000000,
-    "2015/2/18", 5000000,
-    "2015/10/13", 7000000,
-    "2016/2/29", 8000000,
-    "2016/9/8", 10000000,
-    "2016/11/25", 11000000,
-    "2018/3/14", 19000000,
-    "2019/12/14", 30000000,
-    "2021/1/21", 37000000,
-    "2021/5/12", 40000000,
-    "2021/9/22", 42000000,
-    "2021/11/16", 43000000,
-    "2022/2/28", 46000000)
-dates = []
-values = []
-for item in date_values:
-    if type(item) is str:
-        dates.append(item)
-    elif type(item) is int:
-        values.append(item / 10000)
+    ("2012/2/29", 0),
+    ("2013/10/31", 2000000),
+    ("2014/6/11", 3000000),
+    ("2015/2/18", 5000000),
+    ("2015/10/13", 7000000),
+    ("2016/2/29", 8000000),
+    ("2016/9/8", 10000000),
+    ("2016/11/25", 11000000),
+    ("2018/3/14", 19000000),
+    ("2019/12/14", 30000000),
+    ("2021/1/21", 37000000),
+    ("2021/5/12", 40000000),
+    ("2021/9/22", 42000000),
+    ("2021/11/16", 43000000),
+    ("2022/2/28", 46000000),
+    )
+dates, values = zip(*date_values)
 
 x = [dt.datetime.strptime(d, '%Y/%m/%d').date() for d in dates]
 y = [int(v) for v in values]
 
-fig = plt.figure(dpi=200, figsize=(4.0, 2.5))
-ax = fig.add_subplot(1, 1, 1)
-ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
-ax.set_xlim([dt.date(2012, 1, 1), dt.date(2024, 12, 31)])
-ax.set_ylim(0, 5000)
+fig = plt.figure(dpi=300, figsize=(3.0, 2.5), tight_layout=True)
+ax = fig.add_subplot(111)
 
-FONT_NAME1 = "HackGen35"
+fig.patch.set_alpha(0)
 
-plt.rcParams["font.family"] = FONT_NAME1
-plt.rcParams["font.size"] = 9.9
-plt.title('Raspberry Pi 累計出荷台数 (万台)', fontsize=11, fontname=FONT_NAME1)
-plt.rcParams['axes.grid'] = True
-plt.rcParams['axes.linewidth'] = 0.6
-plt.rcParams['grid.linestyle'] = '-'
-plt.rcParams['grid.linewidth'] = 0.8
-plt.rcParams['xtick.direction'] = 'in'
-plt.rcParams['ytick.direction'] = 'in'
-plt.rcParams['xtick.major.width'] = 1.2
-plt.rcParams['ytick.major.width'] = 1.2
+canvas = matplotlib.backends.backend_agg.FigureCanvasAgg(fig)
 
-plt.plot(x, y,  linewidth=1.5,
-         marker="D", markersize=2.5, markeredgewidth=0.5,
-         markeredgecolor="green", markerfacecolor="#a08080")
-plt.show()
+
+# FIG_TITLE = 'Raspberry Pi 累計出荷台数 (百万台)'
+FIG_TITLE = 'Cumulative Shipment Units (mil)'
+UNITS_DIV = 1000000
+UNITS_LIM = 50000000
+
+# FONT_NAME1 = "./Hackgen"
+# ax.set_title(FIG_TITLE, fontsize=8, fontproperties=FONT_NAME1)
+
+ax.set_title(FIG_TITLE, fontsize=8)
+ax.set_xlim([dt.date(2012, 1, 1), dt.date(2023, 1, 31)])
+
+ax.set_ylim(0, UNITS_LIM)
+ax.axes.tick_params(direction='in', width=1.2)
+ax.tick_params(axis="x", labelsize=8, labelrotation=45)
+ax.tick_params(axis="y", labelsize=7.4)
+ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x / UNITS_DIV))))
+ax.axes.grid(visible=True, linestyle='--', linewidth=0.3)
+
+ax.plot(x, y,  linewidth=1.2, color='#8080e0',
+        marker='D', markersize=3.0, markeredgewidth=0.5,
+        markeredgecolor='#80a080', markerfacecolor='#e08080')
+
+# You can show the plot on the display by uncommenting the following line.
+# plt.show()
+
+canvas.print_figure(file_name, dpi=300)
+print(file_name + ' has been saved.')
